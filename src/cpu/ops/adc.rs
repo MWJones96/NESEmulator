5a8f20a67ca8072d31cee4fb153af67a25a6cddb
Matthew Jones <mwjones0896@gmail.com>
2023-03-23 17:21:37 -0400

    ADC Absolute Implemented

--- src/cpu/ops/adc.rs
+++ src/cpu/ops/adc.rs
@@ -13,8 +13,9 @@
     +127 or -128, otherwise overflow is reset. 
 
     The negative flag is set if the accumulator result contains bit 7 on, otherwise the 
-    negative flag is reset. The zero flag is set if the accumulator result is 0, 
-    otherwise the zero flag is reset.
+    negative flag is reset. 
+    
+    The zero flag is set if the accumulator result is 0, otherwise the zero flag is reset.
 */
 
 use super::super::CPU;
@@ -44,8 +45,8 @@ impl CPU {
         1 + self.adc_zp(addr.wrapping_add(self.x), mem)
     }
 
-    pub fn adc_abs(&self) {
-
+    pub fn adc_abs(&mut self, addr: u16, mem: &[u8]) -> u8 {
+        2 + self.adc_imm(mem[addr as usize])
     }
 
     pub fn adc_absx(&self) {
@@ -326,4 +327,108 @@ mod adc_zpx_tests {
 
         assert_eq!(0x10_u8, cpu.a);
     }
+}
+
+#[cfg(test)]
+mod adc_abs_tests {
+    use super::*;
+
+    #[test]
+    fn test_adc_abs_correct_cycles() {
+        let mut cpu = CPU::new();
+
+        assert_eq!(4, cpu.adc_abs(0x0000_u16, &[0x00_u8]));
+    }
+
+    #[test]
+    fn test_adc_abs_fetch_mem_addr() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+        mem[0xffff] = 0x77_u8;
+
+        cpu.adc_abs(0xffff_u16, &mem);
+
+        assert_eq!(0x77_u8, cpu.a);
+    }
+
+    #[test]
+    fn test_adc_abs_carry_flag() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+        mem[0x0] = 0x80_u8;
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(0x80_u8, cpu.a);
+        assert_eq!(false, cpu.c);
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(0x00_u8, cpu.a);
+        assert_eq!(true, cpu.c);
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(0x80_u8, cpu.a);
+        assert_eq!(false, cpu.c);
+    }
+
+    #[test]
+    fn test_adc_abs_zero_flag() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+        mem[0x0] = 0x80_u8;
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(false, cpu.z);
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(true, cpu.z);
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(false, cpu.z);
+    }
+
+    #[test]
+    fn test_adc_abs_negative_flag() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+        mem[0x0] = 0x80_u8;
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(true, cpu.n);
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(false, cpu.n);
+    }
+
+    #[test]
+    fn test_adc_abs_overflow_flag() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+        mem[0x0] = 0x80_u8;
+        mem[0x1] = 0x01_u8;
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(true, cpu.v);
+
+        cpu.adc_abs(0x0, &mem);
+
+        assert_eq!(true, cpu.v);
+
+        cpu.adc_abs(0x1, &mem);
+
+        assert_eq!(false, cpu.v);
+    }
+}
+
+#[cfg(test)]
+mod adc_absx_test {
+
 }
