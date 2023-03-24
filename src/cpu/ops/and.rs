2415fd55e5d6b1018cc1c86bf3433f559aa6ca8e
Matthew Jones <mwjones0896@gmail.com>
2023-03-24 18:07:52 -0400

    AND Absolute Implemented

--- src/cpu/ops/and.rs
+++ src/cpu/ops/and.rs
@@ -19,6 +19,10 @@ impl CPU {
 
         1 + self.and_zp(resolved_addr, mem)
     }
+
+    pub fn and_abs(&mut self, addr: u16, mem: &[u8]) -> u8 {
+        2 + self.and_imm(mem[addr as usize])
+    }
 }
 
 #[cfg(test)]
@@ -179,6 +183,63 @@ mod and_zpx_tests {
         assert_eq!(false, cpu.z);
         cpu.and_zpx(0xff, &[0x00]);
         assert_eq!(true, cpu.z);
+    }
+}
+
+#[cfg(test)]
+mod and_abs_tests {
+    use super::*;
+
+    #[test]
+    fn test_and_abs_correct_cycles() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+
+        assert_eq!(4, cpu.and_abs(0x0, &mem));
+    }
+
+    #[test]
+    fn test_and_abs() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+
+        cpu.a = 0b1010_1010;
+        mem[0x1111] = 0b0101_0101;
+        cpu.and_abs(0x1111, &mem);
+
+        assert_eq!(0x0, cpu.a)
+    }
+
+    #[test]
+    fn test_and_abs_zero_flag() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+
+        cpu.a = 0xff_u8;
+        mem[0x1111] = 0xff_u8;
+        cpu.and_abs(0x1111, &mem);
+
+        assert_eq!(false, cpu.z);
+
+        mem[0x1111] = 0x00_u8;
+        cpu.and_abs(0x1111, &mem);
+
+        assert_eq!(true, cpu.z);
+    }
+
+    #[test]
+    fn test_and_abs_negative_flag() {
+        let mut cpu = CPU::new();
+        let mut mem = [0; 0x10000];
+
+        cpu.a = 0xff_u8;
+        mem[0x1111] = 0xff_u8;
+        cpu.and_abs(0x1111, &mem);
+        assert_eq!(true, cpu.n);
+
+        mem[0x1111] = 0x00_u8;
+        cpu.and_abs(0x1111, &mem);
+        assert_eq!(false, cpu.n);
     }
 }
 
