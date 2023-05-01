24fb9b09254ebc44669b52f93001e674340f1ba8
Matthew Jones <eighthmatthewjones@gmail.com>
2023-05-02 00:30:33 +0100

    Add PPUBus trait

--- src/cpu/bus.rs
+++ src/cpu/bus.rs
@@ -5,3 +5,9 @@ pub trait CPUBus {
     fn read(&self, addr: u16) -> u8;
     fn write(&self, addr: u16, data: u8);
 }
+
+#[automock]
+pub trait PPUBus {
+    fn read(&self, addr: u16) -> u8;
+    fn write(&self, addr: u16, data: u8);
+}
