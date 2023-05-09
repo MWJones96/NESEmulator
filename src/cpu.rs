f2313d34c10aca8f9ec2b65d2df4a94328a04afe
Matthew Jones <eighthmatthewjones@gmail.com>
2023-05-09 20:23:51 +0100

    PLP polls before I flag changed

--- src/cpu.rs
+++ src/cpu.rs
@@ -128,7 +128,7 @@ impl CPU {
                 let i_flag_after = self.i;
 
                 let polled_i_flag = match opcode {
-                    0x58 | 0x78 => i_flag_before,
+                    0x28 | 0x58 | 0x78 => i_flag_before,
                     _ => i_flag_after
                 };
                 //We only poll for interrupts in non-interrupt routines
@@ -862,6 +862,58 @@ mod cpu_tests {
         );
 
         for _ in 0..2 {
+            cpu.clock(&mut bus);
+        }
+
+        //Interrupt triggered immediately (despite I flag set)
+        assert_eq!(
+            CurrentInstruction {
+                remaining_cycles: 7,
+                instruction_type: InstructionType::IRQ
+            },
+            cpu.current_instruction
+        );
+    }
+
+    #[test]
+    fn test_cpu_plp_triggers_interrupt() {
+        let mut cpu = CPU::new();
+        let mut bus = MockCPUBus::new();
+
+        bus.expect_read()
+            .with(eq(CPU::RESET_VECTOR))
+            .once()
+            .return_const(0x40);
+        bus.expect_read()
+            .with(eq(CPU::RESET_VECTOR + 1))
+            .once()
+            .return_const(0x20);
+
+        bus.expect_read().with(eq(0x2040)).once().return_const(0x28); //PLP
+        bus.expect_read().with(eq(0x1fe)).once().return_const(0b1111_1111);
+        
+        bus.expect_read().return_const(0x0);
+
+        cpu.system_irq(true);
+
+        for _ in 0..8 {
+            cpu.clock(&mut bus);
+        }
+
+        cpu.i = false;
+
+        assert_eq!(
+            CurrentInstruction {
+                remaining_cycles: 4,
+                instruction_type: InstructionType::Instruction {
+                    opcode: 0x28,
+                    addressing_mode: cpu.imp()
+                }
+            },
+            cpu.current_instruction
+        );
+
+        for _ in 0..4 {
             cpu.clock(&mut bus);
         }
 
