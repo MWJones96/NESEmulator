73ef5bb264416bab57015a743db8ef44814a9a13
Matthew Jones <eighthmatthewjones@gmail.com>
2023-05-09 21:42:12 +0100

    Add test ROM

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use cpu::CPU;
 mod bus;
 mod cpu;
 
+pub fn get_bytes_from_rom_file(path: String) -> Vec<u8> {
+    Vec::new()
+}
+
 fn main() {
     let mut cpu = CPU::new();
     cpu.system_reset();
