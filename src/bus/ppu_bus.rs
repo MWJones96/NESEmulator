0d7132f4db4870f93f6bd306626d8e6bda65aac9
Matthew Jones <eighthmatthewjones@gmail.com>
2023-06-20 22:46:26 +0100

    Add BG mirroring to PPU Bus palette

--- src/bus/ppu_bus.rs
+++ src/bus/ppu_bus.rs
@@ -9,7 +9,7 @@ pub struct PPUBus<'a> {
     cartridge: Rc<dyn Cartridge + 'a>,
     nametable_0: [u8; 0x400],
     nametable_1: [u8; 0x400],
-    palette: [u8; 0x20],
+    palette: [u8; 0x21],
 }
 
 impl<'a> PPUBus<'a> {
@@ -18,7 +18,7 @@ impl<'a> PPUBus<'a> {
             cartridge,
             nametable_0: [0x0; 0x400],
             nametable_1: [0x0; 0x400],
-            palette: [0x0; 0x20],
+            palette: [0x0; 0x21],
         }
     }
 }
@@ -39,7 +39,14 @@ impl Bus for PPUBus<'_> {
             },
             0x2c00..=0x2fff => self.nametable_1[(addr - 0x2c00) as usize],
             0x3000..=0x3eff => self.read(addr - 0x1000),
-            0x3f00..=0x3fff => self.palette[((addr - 0x3f00) & 0x1f) as usize],
+            0x3f00..=0x3fff => {
+                let mut offset = addr - 0x3f00;
+                if offset % 4 == 0 {
+                    offset = 0;
+                }
+
+                self.palette[(offset & 0x1f) as usize]
+            }
             _ => 0x0, //Open bus read
         }
     }
@@ -59,7 +66,14 @@ impl Bus for PPUBus<'_> {
             },
             0x2c00..=0x2fff => self.nametable_1[(addr - 0x2c00) as usize] = data,
             0x3000..=0x3eff => self.write(addr - 0x1000, data),
-            0x3f00..=0x3fff => self.palette[((addr - 0x3f00) & 0x1f) as usize] = data,
+            0x3f00..=0x3fff => {
+                let mut offset = addr - 0x3f00;
+                if offset % 4 == 0 {
+                    offset = 0;
+                }
+
+                self.palette[(offset & 0x1f) as usize] = data;
+            }
             _ => {} //Open bus write
         }
     }
@@ -226,13 +240,24 @@ mod ppu_bus_tests {
     }
 
     #[test]
-    fn test_read_palette_ram() {
+    fn test_read_from_palette_ram() {
         let cartridge = MockCartridge::new();
         let mut ppu_bus = PPUBus::new(Rc::new(cartridge));
         ppu_bus.palette[0x0] = 0xff;
+        ppu_bus.palette[0x1] = 0xee;
+        ppu_bus.palette[0x2] = 0xdd;
+        ppu_bus.palette[0x3] = 0xcc;
+        ppu_bus.palette[0x4] = 0x00;
+        ppu_bus.palette[0x5] = 0xbb;
 
         assert_eq!(0xff, ppu_bus.read(0x3f00));
+        assert_eq!(0xee, ppu_bus.read(0x3f01));
+        assert_eq!(0xdd, ppu_bus.read(0x3f02));
+        assert_eq!(0xcc, ppu_bus.read(0x3f03));
+        assert_eq!(0xff, ppu_bus.read(0x3f04));
+        assert_eq!(0xbb, ppu_bus.read(0x3f05));
         assert_eq!(0xff, ppu_bus.read(0x3f20));
+        assert_eq!(0xee, ppu_bus.read(0x3f21));
     }
 
     #[test]
@@ -241,8 +266,17 @@ mod ppu_bus_tests {
         let mut ppu_bus = PPUBus::new(Rc::new(cartridge));
 
         ppu_bus.write(0x3f00, 0xff);
-        ppu_bus.write(0x3f20, 0xff);
+        ppu_bus.write(0x3f01, 0xee);
+        ppu_bus.write(0x3f02, 0xdd);
+        ppu_bus.write(0x3f03, 0xcc);
+        ppu_bus.write(0x3f04, 0xbb);
 
+        assert_eq!(0xbb, ppu_bus.palette[0x0]);
+        assert_eq!(0xee, ppu_bus.palette[0x1]);
+        assert_eq!(0xdd, ppu_bus.palette[0x2]);
+        assert_eq!(0xcc, ppu_bus.palette[0x3]);
+
+        ppu_bus.write(0x3f20, 0xff);
         assert_eq!(0xff, ppu_bus.palette[0x0]);
     }
 
--- src/ppu/registers.rs
+++ src/ppu/registers.rs
@@ -1,35 +1,20 @@
+use bitflags::bitflags;
 use std::cell::RefCell;
 
 pub(super) struct Registers {
-    pub _ppu_ctrl: u8,
-    pub _ppu_mask: u8,
-    pub _ppu_status: RefCell<u8>,
-
-    pub _ppu_scroll_x: u8,
-    pub _ppu_scroll_y: u8,
-
-    pub _ppu_addr: RefCell<u16>,
-    pub _ppu_data: u8,
-
-    pub _scroll_latch: RefCell<bool>,
-    pub _addr_latch: RefCell<bool>,
+    pub write_latch: RefCell<bool>,
+    pub loopy_t: u16,
+    pub loopy_v: u16,
+    pub fine_x: u8,
 }
 
 impl Registers {
     pub(super) fn new() -> Self {
         Registers {
-            _ppu_ctrl: 0x0,
-            _ppu_mask: 0x0,
-            _ppu_status: RefCell::new(0x0),
-
-            _ppu_scroll_x: 0x0,
-            _ppu_scroll_y: 0x0,
-
-            _ppu_addr: RefCell::new(0x0),
-            _ppu_data: 0x0,
-
-            _scroll_latch: RefCell::new(false),
-            _addr_latch: RefCell::new(false),
+            write_latch: RefCell::new(false),
+            loopy_t: 0x0,
+            loopy_v: 0x0,
+            fine_x: 0x0,
         }
     }
 }
